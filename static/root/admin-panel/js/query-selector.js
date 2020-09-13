5b9eae2c8f7dd1ea5ec8a9e4a7ab06ad95ecff94
iannisdezwart <iannis__@hotmail.com>
2020-09-13 19:49:03 +0200

    🧹 Cleaned Unnecessary Files

--- static/root/admin-panel/js/query-selector.js
+++
@@ -1,8 +0,0 @@
-const $ = (query) => document.querySelector(query);
-const $a = (query) => Array.prototype.slice.call(document.querySelectorAll(query));
-HTMLElement.prototype.$ = function (query) {
-    return this.querySelector(query);
-};
-HTMLElement.prototype.$a = function (query) {
-    return Array.prototype.slice.call(this.querySelectorAll(query));
-};
