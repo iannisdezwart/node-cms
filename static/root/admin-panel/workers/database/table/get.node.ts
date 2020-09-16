0902ef7d4f0be8d427e4d79c9fd139268af942ba
iannisdezwart <iannis__@hotmail.com>
2020-09-16 15:46:30 +0200

    📑 Added Option to Get Rows Between Given Bounds

--- static/root/admin-panel/workers/database/table/get.node.ts
+++ static/root/admin-panel/workers/database/table/get.node.ts
@@ -3,6 +3,8 @@ import { req, res } from 'apache-js-workers'
 
 const dbName = req.body.dbName as string
 const tableName = req.body.tableName as string
+let from = req.body.from as number
+let to = req.body.to as number
 
 let orderArr = req.body.orderArr as (string | [ string, 'ASC' | 'DESC' ])[]
 if (orderArr == undefined) orderArr = []
@@ -11,9 +13,21 @@ queryTable(
 	dbName,
 	tableName,
 	tableFn => {
-		const table = tableFn.get()
-		const orderedTable = table.orderBy(orderArr)
-		const { rows, cols } = orderedTable
+		let table = tableFn.get()
+
+		// Order table
+
+		if (orderArr.length) table = table.orderBy(orderArr)
+
+		// Get rows between indices
+
+		if (from == null) from = 0
+		if (to == null) to = table.length - 1
+		if (from != 0 || to != table.length - 1) table = table.between(from, to)
+
+		// Send the table
+
+		const { rows, cols } = table
 		const { data } = tableFn
 
 		res.send({ rows, cols, data })
