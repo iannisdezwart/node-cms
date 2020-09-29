480e749df332861cd5f0bf45666c4027825adebb
iannisdezwart <iannis__@hotmail.com>
2020-09-29 19:29:18 +0200

    🛠 Fixed Bug Caused By Legacy Workings of JS

--- static/root/admin-panel/js/admin-panel.ts
+++ static/root/admin-panel/js/admin-panel.ts
@@ -1497,7 +1497,7 @@ const showFiles = (path = '/') => {
 				<h1>Folder: ${ path }</h1>
 
 				<button class="small" onclick="showFiles(upALevel('${ path }'))">Up a level</button>
-				<button class="small" onclick="$('input[type=file]').click()">Upload Files</button>
+				<button class="small" onclick="window.$('input[type=file]').click()">Upload Files</button>
 				<button class="small" onclick="createNewDirectory('${ path }')">New Folder</button>
 				<div class="bulk-actions hidden">
 					Selected Files:
